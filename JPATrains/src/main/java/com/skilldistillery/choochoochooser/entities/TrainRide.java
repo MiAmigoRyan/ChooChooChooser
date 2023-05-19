3907a57e22224cfa5be58802850143a90a0c10ce
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-05-19 10:19:33 -0600

    added trainRide entity and test not null

---
+++ JPATrains/src/main/java/com/skilldistillery/choochoochooser/entities/TrainRide.java
@@ -0,0 +1,55 @@
+package com.skilldistillery.choochoochooser.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="train_ride")
+public class TrainRide {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	public TrainRide() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TrainRide [id=");
+		builder.append(id);
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TrainRide other = (TrainRide) obj;
+		return id == other.id;
+	}
+
+}
