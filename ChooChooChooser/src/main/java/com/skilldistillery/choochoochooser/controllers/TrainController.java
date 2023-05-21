e3f7f436e27bd2ef7966792da43c3730646a7533
patrickhansen <hansen.patrick.d@gmail.com>
2023-05-20 19:20:46 -0600

    Train Controller provides all trains to home

--- ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/TrainController.java
+++ ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/TrainController.java
@@ -3,7 +3,7 @@ package com.skilldistillery.choochoochooser.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.choochoochooser.data.TrainDAO;
 @Controller
@@ -12,10 +12,15 @@ public class TrainController {
 		@Autowired
 		private TrainDAO trainDAO;
 		
-		@GetMapping(path="allTrains.do")
-		public String listAllTrains(Model model) {
+		@RequestMapping(path = { "/", "home.do" })
+		public String listAllTrainsOnHome(Model model) {
 			model.addAttribute("trainList", trainDAO.listAllTrains());
-			return "ShowTrains";	
+			return "home";	
 		}
+		
+		
+//		private String home(Model model) {
+//			return "home";
+//		}
 		
 }
