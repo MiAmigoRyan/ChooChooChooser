b6e3312512f0e2e91692b33a11c03909a76c5468
patrickhansen <hansen.patrick.d@gmail.com>
2023-05-22 15:30:23 -0600

    import on train Controller

--- ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/TrainController.java
+++ ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/TrainController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.choochoochooser.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
