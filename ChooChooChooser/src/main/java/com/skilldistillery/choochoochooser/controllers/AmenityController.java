34bec3cc98fa9775dc96b74a6f27b6fe3825282c
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-05-22 11:09:43 -0600

    added addTrain.do to train controller

--- ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/AmenityController.java
+++ ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/AmenityController.java
@@ -11,6 +11,5 @@ public class AmenityController {
 	
 		@Autowired
 		private AmenityDAO AmenityDAO;
-		
-		
 }
+
--- ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/TrainController.java
+++ ChooChooChooser/src/main/java/com/skilldistillery/choochoochooser/controllers/TrainController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.choochoochooser.data.TrainDAO;
+import com.skilldistillery.choochoochooser.entities.Train;
 import com.skilldistillery.choochoochooser.entities.User;
 @Controller
 public class TrainController {
@@ -27,6 +28,11 @@ public class TrainController {
 			return "ShowTrains";
 		}
 		
+		@RequestMapping(path = {"addTrain.do"})
+		public String addTrain(Train train, Model model) {
+			model.addAttribute("train", trainDAO.addTrain(train));
+			return "detailsPage";
+		}
 //		public void refreshUserInSession(HttpSession session) {
 //			User userInSession = (User) session.getAttribute("loggedInUser");
 //			User loggedInUser = userDAO.findByUsernameAndPassword(userInSession.getUsername(), userInSession.getPassword());
